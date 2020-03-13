d0f211f88da5a31a73d068d346269b30ae4f957d
Tuan Truong <truongtuan97@yahoo.com>
2020-03-13 19:00:03 +0700

    update code

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'phone_number', 'password',
+        'name', 'email', 'password',
     ];
 
     /**
